9589af562e2c003c32b806ec3ce3ff4534642115
abdulqaadirma <abdulqaadir.m.a@gmail.com>
2025-09-25 12:33:10 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+/*
+    Abdulqaadir
+    admin username: admin
+    admin password: wdf#2025
+*/
+
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
